21e9b0c267a6ef58388b8e139e34650feb362ff6
Vicky Das <workmailvicky101@gmail.com>
2021-11-04 18:15:14 +0530

    Added String Reverse algo solution

---
+++ basic-algorithm-scripting/function.js
@@ -0,0 +1,12 @@
+// Reverse a String
+// Reverse the provided string.
+
+// You may need to turn the string into an array before you can reverse it.
+
+// Your result must be a string.
+
+function reverseString(str){
+    str=str.split("").reverse().join("")
+    return str
+}
+console.log(reverseString("Hello"))
