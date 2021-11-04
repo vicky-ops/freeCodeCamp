8e215c716247014f3c0b867e60ac5e7995f1f10c
Vicky Das <workmailvicky101@gmail.com>
2021-11-04 12:35:06 +0530

    Added README.md && copy-an-array-with-the-spread-operator/function.js solution

---
+++ copy-an-array-with-the-spread-operator/function.js
@@ -0,0 +1,11 @@
+function copyMachine(arr,num){
+    let newArr=[]
+    while(num>=1){
+       //Only change code below this line
+        newArr.push([...arr])
+       //Only change code below this line
+        num--;
+    }
+    return newArr;
+}
+console.log(copyMachine([true,false,true],5))
