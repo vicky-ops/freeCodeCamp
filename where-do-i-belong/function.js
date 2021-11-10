ce54a87df248c98eb95843a9d5be98ad5768a38e
Vicky Das <workmailvicky101@gmail.com>
2021-11-10 08:56:56 +0530

    --Added where-do-i-belong

---
+++ where-do-i-belong/function.js
@@ -0,0 +1,7 @@
+// Where do I Belong
+// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+function getIndexToIns(arr,num){
+    // return arr.filter(val=>num>val).length
+    return ["vicky","das"]
+}
+console.log(getIndexToIns([20,3,5],19))
