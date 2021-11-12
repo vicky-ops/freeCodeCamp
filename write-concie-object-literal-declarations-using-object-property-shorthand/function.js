62dd6810cbffd2e200d92c2347cc10b851a2b569
Vicky Das <workmailvicky101@gmail.com>
2021-11-12 09:11:56 +0530

    -Added write-concise-object-literal-declarations-using-object-property-shorthand

---
+++ write-concie-object-literal-declarations-using-object-property-shorthand/function.js
@@ -0,0 +1,12 @@
+// Write Concise Object Literal Declarations Using Object Property Shorthand
+// Use object property shorthand with object literals to create and return an object with name, age and gender properties.
+
+const createPerson=(name,age,gender)={
+
+    return {
+        name,
+        age,
+        gender
+    }
+
+};
