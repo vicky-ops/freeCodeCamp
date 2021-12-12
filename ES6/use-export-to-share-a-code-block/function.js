5b263633ef2b9e3fd396685e4414d1b0670e5d15
Vicky Das <workmailvicky101@gmail.com>
2021-12-12 10:22:44 +0530

    -Added user-export-to-share-a-code-block

---
+++ ES6/use-export-to-share-a-code-block/function.js
@@ -0,0 +1,7 @@
+// Use export to Share a Code Block
+// There are two string-related functions in the editor. Export both of them using the method of your choice.
+
+export const upperCaseString=(string)=>string.toUpperCase()
+export const lowerCaseString=(string)=>string.toLowerCase()
+
+// export {upperCaseString,lowerCaseString}
